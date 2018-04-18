954e7057185dece67ff7f8215672f246f638ed39
Crivella <davidecrivella@yahoo.it>
2018-04-18 10:12:17 +0200

    Added main files

---
+++ main.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+source ./ENVIRONMENT_VARIABLES
+echo "BIN_DIR:" $BIN_DIR
+echo "PSEUDO_DIR:" $PSEUDO_DIR
+echo "TMP_DIR:" $TMP_DIR
+echo "Parallel command:" $RUN_COMMAND
+echo "Started at: " `date`
+
+
+
+
+
+
+
+
+
+echo "End at: $(date)"
+
+
+
+
+exit
+
+
+
+
+
+
+
+
+
